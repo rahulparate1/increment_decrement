8cb130818220ce45ee908886f69f7017bfa24b02
rahulparate-wizgle <rahul.parate@wizgle.com>
2025-06-12 12:53:37 +0530

    changes push

---
+++ new_task/src/app/pages/inc-dec/inc-dec.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChildComponent } from './child/child.component';
+import { ParentComponent } from './parent/parent.component';
+
+
+
+@NgModule({
+  declarations: [
+    ChildComponent,
+    ParentComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class IncDecModule { }
